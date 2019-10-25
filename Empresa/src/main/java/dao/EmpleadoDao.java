20cf6d788b5b555ccde9e3f9f89f89ad0ae00f54
mmb-96 <mmb_96@hotmail.com>
2019-10-25 17:40:11 +0200

    Patron DAO.

---
+++ Empresa/src/main/java/dao/EmpleadoDao.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import laboral.DatosNoCorrectoException;
+import laboral.Empleado;
+
+/**
+ * @author manu
+ *
+ */
+public class EmpleadoDao implements DAO {
+	
+	EmpleadoServicio empServi;
+
+	public EmpleadoDao() {
+		try {
+			empServi = new EmpleadoServicio();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	@Override
+	public List<Empleado> mostarDatosTodos() throws Exception {
+		List<Empleado> empleados;
+		empleados = empServi.mostarDatosTodos();
+		return empleados;
+	}
+
+	@Override
+	public int salaraioDni(String dni) throws Exception {
+		int sueldo;
+		sueldo = empServi.salaraioDni(dni);
+		return sueldo;
+	}
+
+	@Override
+	public boolean actualizarSueldo(String dni) {
+		Boolean vbol;
+		vbol = empServi.actualizarSueldo(dni);
+		return vbol;
+	}
+
+	@Override
+	public boolean actualizarSueldoTodos() {
+		Boolean vbol;
+		vbol = empServi.actualizarSueldoTodos();
+		return vbol;
+	}
+
+	@Override
+	public boolean actualizarNombre(String Dni, String nombre) {
+		Boolean vbol;
+		vbol = empServi.actualizarNombre(Dni, nombre);
+		return vbol;
+	}
+
+	@Override
+	public boolean actualizarSexo(String Dni, char sexo) {
+		Boolean vbol;
+		vbol = empServi.actualizarSexo(Dni, sexo);
+		return vbol;
+	}
+
+	@Override
+	public boolean actualizarCategoria(String Dni, int cat) {
+		Boolean vbol;
+		vbol = empServi.actualizarCategoria(Dni, cat);
+		return vbol;
+	}
+
+	@Override
+	public boolean actualizarAnyos(String Dni, int anyos) {
+		Boolean vbol;
+		vbol = empServi.actualizarAnyos(Dni, anyos);
+		return vbol;
+	}
+
+	@Override
+	public Empleado persona(String dni) throws SQLException, DatosNoCorrectoException {
+		Empleado emp;
+		emp = empServi.persona(dni);
+		return emp;
+	}
+
+}
